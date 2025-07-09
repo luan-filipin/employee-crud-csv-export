9f4e1784d3c2149e8e73946f3115d471d4caf134
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-07-09 13:44:49 -0300

    Teste
    - Criado teste de sucesso e falha para o service findAllEmployees e o findEmployeeByDocument

--- src/test/java/br/com/postgreSQL/service/EmployeeServiceImpTest.java
+++ src/test/java/br/com/postgreSQL/service/EmployeeServiceImpTest.java
@@ -1,5 +1,6 @@
 package br.com.postgreSQL.service;
 
+import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -18,10 +19,15 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import br.com.postgreSQL.model.enums.EmployeeGender;
 import br.com.postgreSQL.dto.EmployeeDto;
 import br.com.postgreSQL.exception.DocumentAlreadyExistsException;
+import br.com.postgreSQL.exception.DocumentNotFoundException;
 import br.com.postgreSQL.mapper.EmployeeMapper;
 import br.com.postgreSQL.model.Address;
 import br.com.postgreSQL.model.Employee;
@@ -74,7 +80,7 @@ public class EmployeeServiceImpTest {
 	}
 
 	@Test
-	public void shouldThrowExceptionWhenDocumentAlreadyExists() {
+	public void shouldThrowException_whenDocumentAlreadyExists() {
 
 		Address address = new Address();
 		address.setStreet("Rua A");
@@ -178,4 +184,110 @@ public class EmployeeServiceImpTest {
 	    
 	    
 	}
+	
+	
+	
+	@Test
+	public void shouldFindEmployeeByDocumentWithSuccessfully() {
+		
+		String document = "11111111111";
+		
+	    Address address = new Address();
+	    address.setStreet("Rua A");
+	    address.setNumber(123L);
+	    address.setCity("Cidade X");
+	    address.setState("Estado Y");
+	    address.setZipCode("12345-678");
+	    address.setCountry("Brasil");
+	    
+	    Employee employee = new Employee(1L, "João Silva", document, LocalDate.of(1990, 1, 1), EmployeeGender.HOMEM, address);
+	    EmployeeDto expectedDto = new EmployeeDto("João Silva", document, LocalDate.of(1990, 1, 1), EmployeeGender.HOMEM, address);
+		
+		doNothing().when(employeeValidator).validateDocumentExists(document);
+		when(employeeRepository.findByDocument(document)).thenReturn(employee);
+		when(employeeMapper.toDto(employee)).thenReturn(expectedDto);
+		
+		EmployeeDto result = employeeServiceImp.findEmployeeByDocument(document);
+		
+		assertNotNull(result);
+		assertEquals(expectedDto, result);
+		assertEquals("João Silva", result.name());
+		assertEquals(document, result.document());
+		
+		verify(employeeValidator).validateDocumentExists(document);
+		verify(employeeRepository).findByDocument(document);
+		verify(employeeMapper).toDto(employee);
+		
+	}
+	
+	@Test
+	public void shouldThrowException_whenDocumentDoesNotExist() {
+		String document = "000000000000";
+		
+		doThrow(new DocumentNotFoundException()).when(employeeValidator).validateDocumentExists(document);
+		
+		assertThrows(DocumentNotFoundException.class, () -> {
+			employeeServiceImp.findEmployeeByDocument(document);
+		});
+		
+		verify(employeeRepository, never()).findByDocument(any());
+		verify(employeeMapper, never()).toDto(any());
+	}
+	
+	@Test
+	public void shouldReturnPagedEmployeesSuccessfully() {
+		
+	    Pageable pageable = PageRequest.of(0, 2);
+
+	    Address address = new Address();
+	    address.setStreet("Rua A");
+	    address.setNumber(123L);
+	    address.setCity("Cidade X");
+	    address.setState("Estado Y");
+	    address.setZipCode("12345-678");
+	    address.setCountry("Brasil");
+	    
+
+	    Employee employee1 = new Employee(1L, "João Silva", "11111111111", LocalDate.of(1990, 1, 1), EmployeeGender.HOMEM, address);
+	    Employee employee2 = new Employee(1L, "Maria Lima", "22222222222", LocalDate.of(1992, 2, 2), EmployeeGender.MULHER, address);
+	    
+	    List<Employee> employeeList = List.of(employee1, employee2);
+	    Page<Employee> employeePage = new PageImpl<>(employeeList);
+	    
+	    EmployeeDto dto1 = new EmployeeDto("João Silva", "11111111111", LocalDate.of(1990, 1, 1), EmployeeGender.HOMEM, address);
+	    EmployeeDto dto2 = new EmployeeDto("Maria Lima", "22222222222", LocalDate.of(1992, 2, 2), EmployeeGender.MULHER, address);
+
+		when(employeeRepository.findAll(pageable)).thenReturn(employeePage);
+		when(employeeMapper.toDto(employee1)).thenReturn(dto1);
+		when(employeeMapper.toDto(employee2)).thenReturn(dto2);
+		
+		Page<EmployeeDto> result = employeeServiceImp.findAllEmployees(pageable);
+		
+		assertNotNull(result);
+		assertEquals(2, result.getContent().size());
+		assertEquals("João Silva", result.getContent().get(0).name());
+		assertEquals("Maria Lima", result.getContent().get(1).name());
+		
+		verify(employeeRepository).findAll(pageable);
+		verify(employeeMapper).toDto(employee1);
+		verify(employeeMapper).toDto(employee2);
+	    
+	}
+	
+	@Test
+	public void shouldThrowException_whenRepositoryFails() {
+		
+		Pageable pageable = PageRequest.of(0, 2);
+		
+		when(employeeRepository.findAll(pageable)).thenThrow(new RuntimeException("Error ao acessar o banco"));
+		
+		RuntimeException exception = assertThrows(RuntimeException.class, ()->{
+			employeeServiceImp.findAllEmployees(pageable);
+		});
+		
+		assertEquals("Error ao acessar o banco", exception.getMessage());
+		
+		verify(employeeMapper, never()).toDto(any());
+	}
+	
 }
